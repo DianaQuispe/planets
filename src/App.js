0ffb0c5391162d2e1918892d85c350f2d0f945b8
DianaQuispe <danaalienmas@gmail.com>
2017-11-14 11:22:11 -0500

    actu

--- src/App.js
+++ src/App.js
@@ -1,34 +1,40 @@
-  import React, { Component } from "react";
-  import "./App.css";
-  import { connect } from "redux-zero/react";
-  import { Grid, Row, Col, Button } from "react-bootstrap";
-  import { HashRouter, Switch, Route, NavLink } from "react-router-dom";
-  import { search, getJSON } from "./actions.js";
+import React, { Component } from "react";
+import "./App.css";
+import { connect } from "redux-zero/react";
+import { Grid, Row, Col, Button } from "react-bootstrap";
+import { HashRouter, Switch, Route, NavLink } from "react-router-dom";
+import { search, getJSON } from "./actions.js";
 
-
-  const App = ({ datos }) => {
-      const mapeo = datos.map((a, b) => {
-        return <div key={b}>
-                    <p>{a.pl_name}</p>
-
-            <p>{a.pl_orbper}</p>
-
-            <p>{a.pl_telescope}</p>
-          </div>;
-      });
-    console.log('datos',datos)
-    getJSON();
+const App = ({ datos }) => {
+  const mapeo = datos.map((a, b) => {
     return <Grid>
         <Row>
-          <Col sm={12}>
-            <button onClick={() => search()}>sera</button>
-            {mapeo}
-            {/* <div> {datos.length != 0 ? <All/> : <br/>}</div> */}
+          <Col sm={10} smOffset={2}>
+            <div key={b}>
+              <p><b>NOMBRE DEL PLANETAS</b> {a.pl_name}</p>
+
+              <p><b>ORBITA</b> {a.pl_orbper}</p>
+
+              <p><b>TELESCOPIO</b>{a.pl_telescope}</p>
+            </div>
           </Col>
         </Row>
       </Grid>;
-  };
+  });
+  console.log("datos", datos);
+  getJSON();
+  return (
+    <Grid className='text-center'>
+      <Row>
+        <Col sm={12}>
+          <Button  onClick={() => search()}>PLANETAS</Button>  <br/>
+          {mapeo}
+        </Col>
+      </Row>
+    </Grid>
+  );
+};
 
-  const mapToProps = ({ datos }) => ({ datos });
+const mapToProps = ({ datos }) => ({ datos });
 
-  export default connect(mapToProps)(App);
+export default connect(mapToProps)(App);
